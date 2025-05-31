162380cef7d70b69512c00896d8ab142e2289964
ra-code422 <rafiatousoumailahamidou@gmail.com>
2025-05-31 16:01:37 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+# Demande du nom du dossier à l'utilisateur
+echo "Entrez le nom du dossier :"
+read dossier
+
+# Vérification si le dossier existe
+if [ -d "$dossier" ]; then
+    # On compte les fichiers dans le dossier (uniquement les fichiers, pas les dossiers)
+    compteur=0
+    for element in "$dossier"/*; do
+        if [ -f "$element" ]; then
+            compteur=$((compteur + 1))
+        fi
+    done
+
+    echo "Le dossier $dossier contient $compteur fichier(s)"
+else
+    echo "Le dossier '$dossier' n'existe pas."
+fi
 
